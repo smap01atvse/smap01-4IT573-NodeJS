4676e569b59471278864ccb403f305d1debc23bd
Patrik Šmátrala <smap01@vse.cz>
2025-03-17 20:21:18 +0100

    Added check for creating files

--- du02/index.mjs
+++ du02/index.mjs
@@ -5,10 +5,10 @@ const filename = "instrukce.txt";
 let num = 0;
 
 async function readNum(filename) {
-    let temp=0;
-    
-    temp = await fs.readFile(filename.trim(), "utf-8");
-    return parseInt(temp);
+  let temp = 0;
+
+  temp = await fs.readFile(filename.trim(), "utf-8");
+  return parseInt(temp);
 }
 
 function createFiles(num) {
@@ -21,8 +21,16 @@ function createFiles(num) {
   return Promise.all(promiseField);
 }
 
-num=await readNum(filename);
+let tempError=null;
+try {
+  num = await readNum(filename);
 
-createFiles(num).then(() => {
-  console.log(`Files created: ${num}`);
-});
+  createFiles(num).then(() => {});
+} catch (error) {
+  console.log(error);
+  tempError = error;
+} finally {
+  if (tempError === null&&!isNaN(num)) {
+    console.log(`Files created: ${num}`);
+  }
+}
